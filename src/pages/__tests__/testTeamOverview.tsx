d3e8cedabece88f149631dc9fcdfca3ae79ee803
Lancy Goyal <i.lancygoyal@gmail.com>
2023-02-14 21:59:19 +0530

    Update testTeamOverview.tsx

--- src/pages/__tests__/testTeamOverview.tsx
+++ src/pages/__tests__/testTeamOverview.tsx
@@ -1,18 +1,19 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import * as React from 'react';
 import {TeamOverview as TeamOverviewType, UserData} from 'types';
-import {render, screen, waitFor} from '@testing-library/react';
+import {render, screen, waitFor, act, fireEvent} from '@testing-library/react';
 import * as API from '../../api';
 import TeamOverview from '../TeamOverview';
 
 jest.mock('react-router-dom', () => ({
     useLocation: () => ({
         state: {
-            name: 'ABC',
+            name: '11',
         },
     }),
     useNavigate: () => ({}),
     useParams: () => ({
-        teamId: '1',
+        teamId: '11',
     }),
 }));
 
@@ -29,30 +30,31 @@ describe('TeamOverview', () => {
         jest.useRealTimers();
     });
 
+    const team = {
+        id: '11',
+        teamLeadId: '1',
+        teamMemberIds: ['2', '3', '4', '5'],
+    };
+
+    const user = id => ({
+        id,
+        firstName: `User ${id}`,
+        lastName: 'Data',
+        displayName: 'userData',
+        location: '',
+        avatar: '',
+    });
+
+    const lead = user(1);
+
+    const users = [user(2), user(3), user(4), user(5)];
+
     it('should render team overview users', async () => {
-        const teamOverview = {
-            id: '1',
-            teamLeadId: '2',
-            teamMemberIds: ['3', '4', '5'],
-        };
-        const userData = {
-            id: '2',
-            firstName: 'userData',
-            lastName: 'userData',
-            displayName: 'userData',
-            location: '',
-            avatar: '',
-        };
-        const users = [
-            {...userData, id: '3'},
-            {...userData, id: '4'},
-            {...userData, id: '5'},
-        ];
         jest.spyOn(API, 'getTeamOverview').mockImplementationOnce(() =>
-            Promise.resolve(teamOverview as TeamOverviewType)
+            Promise.resolve(team as TeamOverviewType)
         );
         jest.spyOn(API, 'getUserData').mockImplementationOnce(() =>
-            Promise.resolve(userData as UserData)
+            Promise.resolve(lead as UserData)
         );
         jest.spyOn(API, 'getUsers').mockImplementationOnce(() =>
             Promise.resolve(users as UserData[])
@@ -61,7 +63,53 @@ describe('TeamOverview', () => {
         render(<TeamOverview />);
 
         await waitFor(() => {
-            expect(screen.queryAllByText('userData')).toHaveLength(4);
+            expect(screen.queryAllByText('userData')).toHaveLength(5);
+        });
+    });
+
+    it('should render search textbox', async () => {
+        jest.spyOn(API, 'getTeamOverview').mockImplementationOnce(() =>
+            Promise.resolve(team as TeamOverviewType)
+        );
+        jest.spyOn(API, 'getUserData').mockImplementationOnce(() =>
+            Promise.resolve(lead as UserData)
+        );
+        jest.spyOn(API, 'getUsers').mockImplementationOnce(() =>
+            Promise.resolve(users as UserData[])
+        );
+
+        await act(async () => {
+            render(<TeamOverview />);
+        });
+
+        const searchbox: HTMLInputElement = screen.getByTestId('textbox-search');
+
+        expect(searchbox).toBeInTheDocument();
+    });
+
+    it('should search for text', async () => {
+        jest.spyOn(API, 'getTeamOverview').mockImplementationOnce(() =>
+            Promise.resolve(team as TeamOverviewType)
+        );
+        jest.spyOn(API, 'getUserData').mockImplementationOnce(() =>
+            Promise.resolve(lead as UserData)
+        );
+        jest.spyOn(API, 'getUsers').mockImplementationOnce(() =>
+            Promise.resolve(users as UserData[])
+        );
+
+        await act(async () => {
+            render(<TeamOverview />);
+        });
+
+        const searchbox: HTMLInputElement = screen.getByTestId('textbox-search');
+
+        fireEvent.change(searchbox, {target: {value: 'user 3'}});
+
+        expect(searchbox.value).toBe('user 3');
+
+        await waitFor(() => {
+            expect(screen.queryAllByText('userData')).toHaveLength(2);
         });
     });
 });
